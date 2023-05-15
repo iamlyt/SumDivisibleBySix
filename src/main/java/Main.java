22b6a2fee685a7b9c16ba9b3c3e02f6bf29780ab
Loi Tran <loitran454@gmail.com>
2023-05-15 02:50:11 -0700

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        // retrieve number of elements in the first line
+        int elements = scanner.nextInt();
+
+        // create array
+        int[] array = new int[elements];
+
+        // create variables to perform operations
+        final int divBySix = 6;
+        int sum = 0;
+
+        // add numbers that are divisible by 6
+        for (int i = 0; i < array.length; i++) {
+            int input = scanner.nextInt();
+            if (input % 6 == 0) {
+                sum += input;
+            }
+        }
+
+        System.out.println(sum);
+
+    }
+}
